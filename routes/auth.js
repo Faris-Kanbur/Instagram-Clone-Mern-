58fca8aa21ce7d3617526c8f8711520f93c6b306
faris <faris38kanbur@gmail.com>
2021-01-14 23:48:36 +0100

    add fixed auth

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,11 @@ router.get('/', (req, res) => {
 })
 
 router.post('/singup', (req,res) => {
-    console.log(req.body.name)
+    const {name,email,password} = req.body
+    if(!email || !password || !name) {
+        return res.status(422).json({error: "please add all the fields"})
+    }
+    res.status(200).json({message: "successfuly posted"})
 })
 
 
